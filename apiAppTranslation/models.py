88e3027e33941ac948d1d662da3c1430418b9794
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2024-04-12 10:26:38 +0530

    Update models.py

--- apiAppTranslation/models.py
+++ apiAppTranslation/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class TextModel(models.Model):
     
-    data = models.TextField() 
+    data = models.TextField()
+    sourceLanguageCode = models.CharField(max_length=255) 
     targetLanguage = models.CharField(max_length=255)
     targetLanguageCode = models.CharField(max_length=255)
     
@@ -14,4 +15,13 @@ class FileModel(models.Model):
     
     file = models.FileField(upload_to="uploads/")
     targetLanguage = models.CharField(max_length=255)
+
+    
+
+class PdfFileModel(models.Model):
+    
+    file = models.FileField(upload_to="uploads/")
+    targetLanguage = models.CharField(max_length=255)
+    sourceLanguage = models.CharField(max_length=255)
+    
     
