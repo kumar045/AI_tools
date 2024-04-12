987c00655567653e71e5c7cb99a8e70e00e0db60
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2024-04-12 10:28:33 +0530

    Update urls.py

--- apiAppTranslation/urls.py
+++ apiAppTranslation/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('text/', views.TextConversion.as_view(), name="Text_Conversion"),
-    path('file/', views.FileConversion.as_view(), name="File_Conversion")
+    path('file/', views.FileConversion.as_view(), name="File_Conversion"),
+    path('pdf2pdf/', views.PdfFileConversion.as_view(), name="Pdf_File_Conversion"),
+    
 ]
